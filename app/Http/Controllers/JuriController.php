dc34bff3a5443648c6cf78ee349e8224692783d4
manggala56 <manggalaputra740@gmail.com>
2023-12-28 05:32:55 +0700

    new juri solo query

--- app/Http/Controllers/JuriController.php
+++ app/Http/Controllers/JuriController.php
@@ -77,35 +77,6 @@ class JuriController extends Controller
                 ];
                 score::create($data);
             }
-            elseif($keterangan === "senidewan"){
-                    $data = [
-                        'score' => $p,
-                        'keterangan' => $status,
-                        'id_perserta' => $id_perserta,
-                        'id_juri' => $id_juri,
-                        'status' => 'plus'
-                    ];
-            }
-            elseif($keterangan === "pointseni"){
-                $check = [
-                    'id_perserta' => $id_perserta,
-                    'keterangan' => $status,
-                ];
-                $data = [
-                    'score' => $p,
-                    'keterangan' => $status,
-                    'id_perserta' => $id_perserta,
-                    'id_juri' => $id_juri,
-                    'status' => 'point_solo'
-                ];
-                $datas = score::where($check)->first();
-                if ($datas) {
-                    $datas->update($data);
-                } else {
-                    score::create($data);
-                }
-                return response()->json(['message' => 'Data berhasil disimpan']);
-        }
     }
 
         public function stream()
