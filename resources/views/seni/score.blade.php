43262afffa7ae3fdb1cc7dec641cbbbea4a3ed4d
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2023-12-29 18:03:35 +0700

    create admin panel

--- resources/views/seni/score.blade.php
+++ resources/views/seni/score.blade.php
@@ -131,6 +131,7 @@
             </div>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         function calldata() {
             function attack() {
