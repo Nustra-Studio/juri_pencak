aa43f8ab5c6f14b589eb44b0ca1e87ffdc68db0d
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-02 15:04:13 +0700

    button

--- resources/views/penilaian/juri.blade.php
+++ resources/views/penilaian/juri.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Pencak Silat</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/juri/style.css">
+        <link rel="stylesheet" href="../assets/juri/style2.css">
         @php
             $id_juri = 2;
             use App\score;
