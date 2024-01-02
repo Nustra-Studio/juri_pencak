41f40fe162433fa9eae77835e3aff21f547b767c
manggala56 <manggalaputra740@gmail.com>
2024-01-02 18:30:59 +0700

    new babak dewan

--- resources/views/penilaian/dewan.blade.php
+++ resources/views/penilaian/dewan.blade.php
@@ -316,7 +316,7 @@
                                     <tr>
                                         <td style="" >{{$item->id_juri}}</td>
                                         @php
-                                                $biru = ;
+                                                $biru = '2' ;
                                                 if ($item->id_perserta == $biru) {
                                                 $color = "color: rgba(0, 102, 255, 1)";
                                                 $text = 'tim1';
