329ee306d613f7848dbd19ac4f1ed28698e8e25b
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-02 20:32:19 +0700

    Juri tungal

--- resources/views/seni/tunggal/juri.blade.php
+++ resources/views/seni/tunggal/juri.blade.php
@@ -8,55 +8,88 @@
     <title>Juri Seni</title>
 </head>
 <body>
-        <div class="d-flex justify-content-center ">
-            <div class="mid-header-text" >
-                Juri Pencak Seni Tunggal
+           <!-- Match Info Section -->
+           <div class="d-flex justify-content-center ">
+            <div class="mid-header-text text-center" >
+                ARENA 1 <br>
+                PENYISIHAN - DEWASA (TUNGGAL)
             </div>
         </div>
-        <style>
-            .custom-button {
-            height: 289px;
-            width: 100%;
-            }
-        </style>
-        <div class="container mt-5" >   
-            <div class="row text-center">
-                <div class="col-md-5"  >
+        <!-- Player Info Section -->
+        <div class="container mt-3" >   
+            <div class="row">
+                <div class="col">
+                    Nama : <br>
+                    <span class="text-primary fw-bold fs-5">Brian Putra Imanuel</span>
+                </div>
+                <div class="col text-end">
+                    : Kontingen <br>    
+                    <span class="text-primary fw-bold fs-5">KEDIRI</span>
+                </div>
+            </div>
+            <!-- Score Section -->
+            <div class="text-center border border-black rounded py-1">Tunggal Jurus 1 Tangan Kosong Movement 1</div>
+            <div class="row text-center mt-4">
+                <div class="col-md-5">
                     <button class="btn btn-danger btn-lg custom-button shadow">Wrong Move</button>
                 </div>
-                <div class="col-md-2 ">
+                <div class="col-md-2">
+                    <div class="container-fluid h-100">
+                        <div class=" h-100 d-flex justify-content-between align-items-center">
+                            <table class="table table-bordered border-black">
+                                <thead>
+                                    <tr>
+                                        <th colspan="2" class="fw-bold" style="color: rgb(190, 190, 0);">SCORE</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-danger fw-bold">0</td>
+                                        <td class="text-primary fw-bold">0</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-md-5" >
                     <button class="btn btn-primary btn-lg custom-button shadow">Next Move</button>
                 </div>
             </div>
         </div>
-        
-        <div class="d-flex justify-content-center direc mt-5">
-            <div class=" text-center " >
-                Flow of Movement/Stamina <br>
-                (Range Score : 0,01 - 0,10)
-            </div>
+        <div class="d-flex justify-content-center direc mt-4">
             <div class=" text-center container " >
                 <div class="row">
-                    <table class="table table-borderd ">
+                    <table class="table table-bordered border-black rounded">
                         <thead>
                             <tr>
-                                <td scope="col"></td>
+                                <th class="bg-dark-subtle">ACCURACY TOTAL SCORE</th>
+                                <th class="text-primary fw-bold">9.92</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="">
-                                <td scope="row" class="d-flex justify-content-start">
+                            <tr>
+                                <td scope="row"> 
+                                    Flow of Movement/Stamina (Range Score : 0,01 - 0,10)
+                                    <div></div>
                                     <!-- Increment 0,01 jadi 10 button 0,01 sampai 0,10 -->
-                                    @for ($i = 1; $i <= 10; $i++)
-                                    @php
-                                        $number = number_format($i * 0.01, 2);
-                                    @endphp
-                                    <button class="btn btn-primary btn-lg mx-1">{{$number}}</button>
-                                    @endfor
-                                    <span class="ms-auto mt-2 span-score ">0,01</span>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1t">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
+                                    <button class="btn btn-primary btn-lg mx-1">0,01</button>
                                 </td>
+                                <td class="text-center align-middle text-primary fw-bold">
+                                    0.01
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="bg-dark-subtle fw-bold">TOTAL SCORE :</td>
+                                <td class="fw-bold text-primary">9.93</td>    
                             </tr>
                         </tbody>
                     </table>
