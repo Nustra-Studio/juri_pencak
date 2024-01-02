cfb66c4c83251398ecea32dc0cd5c426542280e9
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-02 13:37:24 +0700

    button

--- resources/views/penilaian/juri.blade.php
+++ resources/views/penilaian/juri.blade.php
@@ -85,8 +85,8 @@
                 </table>
                 <div class="d-flex justify-content-between">
                     <div class="btn-wrap d-flex flex-column">
-                        <button name="juri:{{$id_juri}} id:1 status:pukulan p:2 keterangan:plus" class="btnSkill1 d-flex align-items-center justify-content-center ">
-                        <img src="./assets/juri/images/fist.png">
+                        <button name="juri:{{$id_juri}} id:1 status:pukulan p:2 keterangan:plus" class="btnSkill1 d-flex align-items-center justify-content-center btn btn-primary fs-2 py-4 me-1 shadow border-black">
+                        <img src="../assets/juri/images/fist.png">
                         <span>Pukulan</span>
                         </button>  
                         
