37821b34e0822769dd57db9682a469623a003128
manggala56 <manggalaputra740@gmail.com>
2024-01-02 18:49:53 +0700

    new babak dewan

--- resources/views/penilaian/dewan.blade.php
+++ resources/views/penilaian/dewan.blade.php
@@ -293,7 +293,7 @@
             disabled
         @endif>PERINGATAN</button>
         </div>
-        <div class="modal fade" id="vjatuhan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal fade" id="vjatuhan" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropJatuhan" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -335,7 +335,7 @@
                     </div>
               </div>
         </div>
-        <div class="modal fade" id="vhukuman" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabelhukuman" aria-hidden="true">
+        <div class="modal fade" id="vhukuman" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"  aria-labelledby="staticBackdropLabelHukuman" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
