0758f597e22ccaf1d6df42ee35d49bfc61e5d9fe
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-02 14:16:21 +0700

    dewan solo detail

--- resources/views/penilaian/juri.blade.php
+++ resources/views/penilaian/juri.blade.php
@@ -86,12 +86,12 @@
                 <div class="d-flex justify-content-between">
                     <div class="btn-wrap d-flex flex-column">
                         {{-- btnSkill1 --}}
-                        <button name="juri:{{$id_juri}} id:1 status:pukulan p:2 keterangan:plus" class=" d-flex align-items-center justify-content-center btn btn-primary fs-5 py-5 px-0 me-1 shadow border-black" >
+                        <button name="juri:{{$id_juri}} id:1 status:pukulan p:2 keterangan:plus" class=" d-flex align-items-center justify-content-center btn btn-primary fs-5 py-5 px-0 px-lg-5 me-1 shadow border-black" >
                         <img src="../assets/juri/images/fist.png" style="width: 20px;">
                         <span class="ms-1">Pukulan</span>
                         </button>  
                         
-                        <button name="juri:{{$id_juri}} id:1 status:tendangan p:3 keterangan:plus" class=" d-flex align-items-center justify-content-center btn btn-primary fs-5 py-5 px-0 me-1 shadow border-black" >
+                        <button name="juri:{{$id_juri}} id:1 status:tendangan p:3 keterangan:plus" class=" d-flex align-items-center justify-content-center btn btn-primary fs-5 py-5 px-0 px-lg-5 me-1 shadow border-black" >
                             <img src="../assets/juri/images/kick.png" style="width3 20px;">
                             <span class="ms-1">Tendangan</span>
                         </button>
