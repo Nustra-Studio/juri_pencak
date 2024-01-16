e52b90b51b0bab2ea051462ca4c4c1678c1fd182
wiryas999 <yasawira06@gmail.com>
2024-01-16 08:40:43 +0700

    new arena page

--- app/arena.php
+++ app/arena.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class arena extends Model
 {
     use HasFactory;
+    protected $table = 'arenas';
+    protected $fillable =[
+        'name',
+        'status',
+        'keterangan',
+    ];
 }
--- resources/views/admin/arena.blade.php
+++ resources/views/admin/arena.blade.php
@@ -8,15 +8,47 @@
     @php
         use App\PersertaModel;
         use App\Setting;
+        use App\arena;
+        $data = arena::all();
         $data_setting = Setting::first();
         $data_perserta = PersertaModel::get();
+
     @endphp
     <div class="row">
         <div class="col">
             <div class="card">
-            <div class="card-body">
-                
-            </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                    <table id="dataTableExample" class="table">
+                        <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody id="tb-category">
+                            @foreach ($data as $item)
+                            
+                            <tr>
+                                <td>{{ $loop->index+1 }}</td>
+                                <td> {{$item->name}}</td>
+                                <td>
+                                <div class="text-end">
+                                    <a href="" class="btn btn-primary btn-sm">Edit</a>
+                                    {{-- <form id="form-delete-{{ $item->id }}" action="{{ route('category.destroy', $item->id) }}" method="POST" style="display: none;">
+                                    @csrf
+                                    @method('DELETE')
+                                </form> --}}
+                                <button class="btn btn-danger btn-sm delete-button" data-form-delete="{{ $item->id }}">Delete</button>
+                                </div>
+                            </tr>
+                        @endforeach 
+                        
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
