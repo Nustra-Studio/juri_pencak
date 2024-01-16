b34af374f98d208b203946985f9f10299f6c3b41
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-16 20:11:08 +0700

    timer

--- resources/views/penilaian/score.blade.php
+++ resources/views/penilaian/score.blade.php
@@ -54,7 +54,7 @@
                 ARENA 1 <br>
                 PENYISIHAN - DEWASA (4 PA) <br>
                 <div id=""name="" class="mid-container-timer">
-                    <div id=""name="timer" class="p-1 fs-4 px-3 bg-black border border-dark text-light">
+                    <div id=""name="timer" class="py-0 fs-4 px-3 bg-black border border-primary text-light rounded">
                         03:00
                     </div>
                 </div>
