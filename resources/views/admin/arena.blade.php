5009b2223d5250ac4fdf74223a511f1e0ad70f3a
wiryas999 <yasawira06@gmail.com>
2024-01-16 08:43:16 +0700

    new arena page

--- resources/views/admin/arena.blade.php
+++ resources/views/admin/arena.blade.php
@@ -17,6 +17,9 @@
     <div class="row">
         <div class="col">
             <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Data Category</h4>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#adddata">Tambah Data</button>
                 <div class="card-body">
                     <div class="table-responsive">
                     <table id="dataTableExample" class="table">
@@ -36,10 +39,10 @@
                                 <td>
                                 <div class="text-end">
                                     <a href="" class="btn btn-primary btn-sm">Edit</a>
-                                    {{-- <form id="form-delete-{{ $item->id }}" action="{{ route('category.destroy', $item->id) }}" method="POST" style="display: none;">
+                                    <form id="form-delete-{{ $item->id }}" action="" method="POST" style="display: none;">
                                     @csrf
                                     @method('DELETE')
-                                </form> --}}
+                                </form>
                                 <button class="btn btn-danger btn-sm delete-button" data-form-delete="{{ $item->id }}">Delete</button>
                                 </div>
                             </tr>
@@ -49,6 +52,43 @@
                     </table>
                     </div>
                 </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="adddata" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Data Category</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="forms-sample">
+                        <div class="mb-3">
+                            <label for="exampleInputUsername1" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+                        </div>
+                        <div class="form-check mb-3">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">
+                                Remember me
+                            </label>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </form>
+                </div>
+            </div>
             </div>
         </div>
     </div>
