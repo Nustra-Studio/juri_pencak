df66f76bcbea51ded36b745891d5da9c01805271
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 13:30:27 +0700

    new score

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -2,6 +2,9 @@
 
 @push('plugin-styles')
   <link href="{{ asset('assets/plugins/flatpickr/flatpickr.min.css') }}" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
   <style>
           .form-floating {
           position: relative;
