922ac5a4c1edd8cb6b085486ca90f9115bb6f96f
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 15:57:06 +0700

    new score

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -126,16 +126,22 @@
                   <td class="fw-bold text-primary">23</td>
                   <td class="fw-bold">Galang</td>
                   <td class="h-100 px-0 py-0 w-25">
-                      <div class="container form-group p-0 " >
-                          <select class="custom-select w-75 p-0" id="input-continent" style="height: 60px;">
-                              <option value="menang-1">Menang Point</option>
-                              <option value="menang-2">Menang Teknik</option>
-                              <option value="menang-2">Diskualifikasi</option>
-                              <option value="menang-3">Lawan Mengundurkan diri</option>
-                              <option value="menang-4">Keputusan wasit</option>
-                          </select>
-                      </div>
-                      <button class="btn btn-success">K</button>
+                    <div class="row">
+                        <div class="col">
+                            <div class="container form-group p-0 " >
+                                <select class="custom-select w-75 p-0" id="input-continent" style="height: 60px;">
+                                    <option value="menang-1">Menang Point</option>
+                                    <option value="menang-2">Menang Teknik</option>
+                                    <option value="menang-2">Diskualifikasi</option>
+                                    <option value="menang-3">Lawan Mengundurkan diri</option>
+                                    <option value="menang-4">Keputusan wasit</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-success">K</button>
+                        </div>
+                    </div>
                   </td>
                   <td>
                       <div class="d-flex justify-content-center p-0">
