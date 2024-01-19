7a24cbd5737ddee1f527cf0e4a73646ef9017885
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 16:24:43 +0700

    score

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -124,7 +124,7 @@
                   <td class="fw-bold text-primary">Galang</td>
                   <td class="fw-bold text-danger">12</td>
                   <td class="fw-bold text-primary">23</td>
-                  <td class="fw-bold">Galang</td>
+                  <td class="fw-bold">Tim Biru</td>
                   <td class="h-100 px-0 py-0 w-25">
                     <div class="form-group p-0 d-flex justify-content-start align-items-center flex-row">
                         <select class="custom-select w-100 p-0 mx-1" id="input-continent" style="height: 55px;">
