23792ef4574a89a796ee7811a56c2184f30a2ed4
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 16:33:52 +0700

    score

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -125,7 +125,7 @@
                   <td class="fw-bold text-danger">12</td>
                   <td class="fw-bold text-primary">23</td>
                   <td class="fw-bold p-0 h-100">
-                    <select class="custom-select w-100 p-0 h-100" id="input-continent">
+                    <select class="custom-select w-100 p-0" id="input-winner"  style="height: 70px;">
                         <option value="peserta-1">Sudut Biru</option>
                         <option value="peserta-2">Sudut Merah</option>
                         <option value="peserta-3">N/A</option>
