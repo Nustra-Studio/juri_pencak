b373bb303443f2edb595bea96fb3a22cebcdf207
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 10:56:40 +0700

    dewan

--- resources/views/admin/kontigen.blade.php
+++ resources/views/admin/kontigen.blade.php
@@ -7,14 +7,14 @@
 
 @section('content')
 @php
-    use App\category;
-    $data = category::get();
+    use App\KontigenModel;
+    $data = KontigenModel::get();
 @endphp
 <div class="row">
     <div class="col">
         <div class="card">
             <div class="card-header">
-                <h4 class="card-title">Data Category</h4>
+                <h4 class="card-title">Data Kontigen</h4>
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#adddata">Tambah Data</button>
             <div class="card-body">
                 <div class="table-responsive">
@@ -31,14 +31,14 @@
                         
                         <tr>
                             <td>{{ $loop->index+1 }}</td>
-                            <td> {{$item->name}}</td>
+                            <td> {{$item->kontigen}}</td>
                             <td>
                             <div class="text-end">
                                 <a href="" class="btn btn-primary btn-sm">Edit</a>
-                                <form id="form-delete-{{ $item->id }}" action="{{ route('category.destroy', $item->id) }}" method="POST" style="display: none;">
+                                {{-- <form id="form-delete-{{ $item->id }}" action="{{ route('category.destroy', $item->id) }}" method="POST" style="display: none;">
                                 @csrf
                                 @method('DELETE')
-                            </form>
+                            </form> --}}
                             <button class="btn btn-danger btn-sm delete-button" data-form-delete="{{ $item->id }}">Delete</button>
                             </div>
                         </tr>
