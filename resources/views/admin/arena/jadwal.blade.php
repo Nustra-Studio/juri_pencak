74751ff5573caaef5656f838dda5e7c225d31d5d
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-01-19 16:04:45 +0700

    new score

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -128,7 +128,7 @@
                   <td class="h-100 px-0 py-0 w-25">
                     <div class="row w-100 gap-0">
                         <div class="col m-0 p-0">
-                            <div class="container form-group p-0 ">
+                            <div class="form-group p-0 ">
                                 <select class="custom-select w-100 p-0" id="input-continent" style="height: 60px;">
                                     <option value="menang-1">Menang Point</option>
                                     <option value="menang-2">Menang Teknik</option>
