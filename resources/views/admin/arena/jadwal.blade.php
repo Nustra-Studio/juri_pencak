13400440560c0e402b9fb302dba5a38d8dc3502b
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-03-15 10:07:57 +0700

    jadwal

--- resources/views/admin/arena/jadwal.blade.php
+++ resources/views/admin/arena/jadwal.blade.php
@@ -86,54 +86,53 @@
     </div>
 
     @if ($tipe_arena->status === "Tanding")
-    <div class="container">a</div>
-    @else
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="table-responsive-lg">  
+                <div class="table-responsive-lg">
                     <div class="card-body">
-                        <table id="example" class="table table-bordered shadow" style="width: 100%;">
+                        <table id="example"class="table table-bordered shadow" style="width: 100%;">
                             <thead class="text-center">
-                                <tr>    
+                                <tr>
                                     <th class="bg-light text-center">No</th>
                                     <th class="bg-light text-center">Partai</th>
                                     <th class="bg-light text-center">Kelas</th>
-                                    <th class="bg-light text-center">Nama</th>
-                                    <th class="bg-light text-center">Kategori</th>
+                                    <th class="bg-light text-center">Sudut Merah</th>
+                                    <th class="bg-light text-center">Sudut Biru</th>
                                     <th class="bg-light text-center">Skor</th>
-                                    <th class="bg-light text-center">Waktu</th>
-                                    <th class="bg-light text-center">Deviation</th>
+                                    <th class="bg-light text-center">Skor</th>
+                                    <th class="bg-light text-center">Pemenang</th>
                                     <th class="bg-light text-center">Kondisi Menang</th>
                                     <th class="bg-light text-center">Status</th>
                                 </tr>
                             </thead>
                             <tbody class="text-center align-middle">
-                                    @foreach ($data_perserta as $item)
-                                    <tr>
-                                            @php
-                                                $kelas = kelas::where('id',$item->kelas)->value('name');
-                                                $category = category::where('id',$item->category)->value('name')
-                                            @endphp
-                                        <td class="text-center">{{ $loop->index+1 }}</td>
-                                        <td class="text-center">1</td>
-                                        <td>{{$kelas}}</td>
-                                        <td class="">{{$item->name}}</td>
-                                        <td class="">{{$category}}</td>
-                                        <td class="text-danger text-center">N/A</td>
-                                        <td class="text-primary text-center">N/A</td>
-                                        <td class="text-center">N/A</td>
-                                        <td class="h-100 px-0 py-0">
-                                            <div class="container form-group p-0 " >
-                                                <select class="form-select w-100 p-0 text-center"" id="input-continent" style="height: 60px;">
-                                                    <option value="menang-1">Menang Point</option>
-                                                    <option value="menang-2">Menang Teknik</option>
-                                                    <option value="menang-2">Diskualifikasi</option>
-                                                    <option value="menang-4">Keputusan wasit</option>
-                                                </select>
-                                            </div>
-                                        </td>
-
+                                @foreach ($data_peserta as $item)
+                                @php    
+                                    $kelas = kelas::where('id',$item->kelas)->('name', 'LIKE', '%' . 'tanding' . '%' )->value('name');
+                                    $category = category::where('id',$item->category)->value('name');
+                                    $lawan = PesertaModel::where('id', $item->id_pesilat+1)->value('name');
+                                @endphp
+                                <tr>
+                                    <td class="text-center">{{ $loop->index+1 }}</td>
+                                    <td class="text-center">1</td>
+                                    <td>{{ $kelas }}</td>
+                                    <td class="fw-bold text-danger">{{ $item->name}}</td>
+                                    <td class="fw-bold text-primary">{{$lawan}}</td>
+                                    <td class="fw-bold text-danger text-center">N/a</td>
+                                    <td class="fw-bold text-primary text-center">N/a</td>
+                                    <td class="fw-bold">N/a</td>
+                                    <td class="h-100 px-0 py-0 w-25">
+                                        <div class="container form-group p-0 " >
+                                            <select class="custom-select w-100 p-0" id="input-continent" style="height: 60px;">
+                                                <option value="menang-1">Menang Point</option>
+                                                <option value="menang-2">Menang Teknik</option>
+                                                <option value="menang-2">Diskualifikasi</option>
+                                                <option value="menang-3">Lawan Mengundurkan diri</option>
+                                                <option value="menang-4">Keputusan wasit</option>
+                                            </select>
+                                        </div>
+                                    </td>
                                     <td>
                                         <div class="d-flex justify-content-center p-0">
                                             @if ($item->status === "pending")
@@ -149,13 +148,82 @@
                                     </td>
                                 </tr>
                                 @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
+        
     </div>
+    @else
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="table-responsive-lg">  
+                        <div class="card-body">
+                            <table id="example" class="table table-bordered shadow" style="width: 100%;">
+                                <thead class="text-center">
+                                    <tr>    
+                                        <th class="bg-light text-center">No</th>
+                                        <th class="bg-light text-center">Partai</th>
+                                        <th class="bg-light text-center">Kelas</th>
+                                        <th class="bg-light text-center">Nama</th>
+                                        <th class="bg-light text-center">Kategori</th>
+                                        <th class="bg-light text-center">Skor</th>
+                                        <th class="bg-light text-center">Waktu</th>
+                                        <th class="bg-light text-center">Deviation</th>
+                                        <th class="bg-light text-center">Kondisi Menang</th>
+                                        <th class="bg-light text-center">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center align-middle">
+                                        @foreach ($data_perserta as $item)
+                                        <tr>
+                                                @php
+                                                    $kelas = kelas::where('id',$item->kelas)->value('name');
+                                                    $category = category::where('id',$item->category)->value('name')
+                                                @endphp
+                                            <td class="text-center">{{ $loop->index+1 }}</td>
+                                            <td class="text-center">1</td>
+                                            <td>{{$kelas}}</td>
+                                            <td class="">{{$item->name}}</td>
+                                            <td class="">{{$category}}</td>
+                                            <td class="text-danger text-center">N/A</td>
+                                            <td class="text-primary text-center">N/A</td>
+                                            <td class="text-center">N/A</td>
+                                            <td class="h-100 px-0 py-0">
+                                                <div class="container form-group p-0 " >
+                                                    <select class="form-select w-100 p-0 text-center"" id="input-continent" style="height: 60px;">
+                                                        <option value="menang-1">Menang Point</option>
+                                                        <option value="menang-2">Menang Teknik</option>
+                                                        <option value="menang-2">Diskualifikasi</option>
+                                                        <option value="menang-4">Keputusan wasit</option>
+                                                    </select>
+                                                </div>
+                                            </td>
+                                        <td>
+                                            <div class="d-flex justify-content-center p-0">
+                                                @if ($item->status === "pending")
+                                                <button name="keterangan:jadwal p:{{$item->id}} status:proses arena:{{$arena}}" class="btn btn-data btn-primary px-3 shadow text-light">Pending</button>
+                                                @endif
+                                                @if ($item->status === "proses")
+                                                <button name="keterangan:jadwal p:{{$item->id}} status:finish arena:{{$arena}}" class="btn btn-data btn-warning px-3 shadow text-light">Proses</button>
+                                                @endif
+                                                @if ($item->status === "finish")
+                                                <button name="keterangan:jadwal p:{{$item->id}} status:finish arena:{{$arena}}" class="btn btn-success px-3 shadow text-light">Selesai</button>
+                                                @endif
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     @endif
     <!-- Information Table -->
     
